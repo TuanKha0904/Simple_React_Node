9062405fde194ff333dc7b5e4208f94f4f0e4dc5
TuanKha0904 <94231162+TuanKha0904@users.noreply.github.com>
2024-05-14 23:14:35 +0700

    feat: connect with database

--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,15 @@
 const express = require("express");
+const mysql = require("mysql2");
 const app = express();
+const connectDB = async () => {
+    mysql.createConnection({
+        host: "localhost:3306",
+        user: "root",
+        password: "root123",
+        database: "Simple"
+    });
+}
+connectDB();
 app.get("/", (req, res) => {
     res.send("Hello World!");
 });
